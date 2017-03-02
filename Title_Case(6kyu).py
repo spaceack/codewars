540d416a9d45a925853ba01ae4f0ea4175123525
spaceack <spaceack@gmail.com>
2017-03-02 13:06:51 +0800

    add test function

--- Title_Case(6kyu).py
+++ Title_Case(6kyu).py
@@ -40,3 +40,29 @@ def initial_upper(word):
         for t in range(1, len(word)):
             new_word = new_word + word[t].lower()
     return new_word
+
+def test(title, minor_words, expect):
+    answer = title_case(title) if minor_words is None else title_case(title, minor_words)
+    if answer == expect:
+        result = 'AC! '
+    else:
+        result = 'Error! '
+
+    message = "Gave title={0}{1}. Expected {2} but got {3}.".format(
+        repr(title),
+        '' if minor_words is None else ', minor_words=' + repr(minor_words),
+        repr(expect),
+        repr(answer))
+    print(result + message)
+    # Test.expect(answer == expect, message)
+
+
+if __name__ == '__main__':
+    test('', '', '')
+    test('aBC deF Ghi', None, 'Abc Def Ghi')
+    test('ab', 'ab', 'Ab')
+    test('a bc', 'bc', 'A bc')
+    test('a bc', 'BC', 'A bc')
+    test('First a of in', 'an often into', 'First A Of In')
+    test('a clash of KINGS', 'a an the OF', 'A Clash of Kings')
+    test('the QUICK bRoWn fOX', 'xyz fox quick the', 'The quick Brown fox')
